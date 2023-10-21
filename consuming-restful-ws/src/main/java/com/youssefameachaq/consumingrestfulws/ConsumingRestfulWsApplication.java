32bc2d0d5834f440980ded609d5ccf81befdd768
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-21 16:19:24 +0200

    Consuming a RESTful Web Service : setup the project

---
+++ consuming-restful-ws/src/main/java/com/youssefameachaq/consumingrestfulws/ConsumingRestfulWsApplication.java
@@ -0,0 +1,13 @@
+package com.youssefameachaq.consumingrestfulws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestfulWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestfulWsApplication.class, args);
+	}
+
+}
---
+++ consuming-restful-ws/src/test/java/com/youssefameachaq/consumingrestfulws/ConsumingRestfulWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssefameachaq.consumingrestfulws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestfulWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
