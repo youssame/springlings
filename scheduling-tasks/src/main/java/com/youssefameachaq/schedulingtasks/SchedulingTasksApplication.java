120f21354d1676d0ec1c83c69bf45dfd405214e2
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-21 13:41:14 +0200

    Scheduling Tasks : Enable Scheduling

--- scheduling-tasks/src/main/java/com/youssefameachaq/schedulingtasks/SchedulingTasksApplication.java
+++ scheduling-tasks/src/main/java/com/youssefameachaq/schedulingtasks/SchedulingTasksApplication.java
@@ -2,8 +2,10 @@ package com.youssefameachaq.schedulingtasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingTasksApplication {
 
 	public static void main(String[] args) {
