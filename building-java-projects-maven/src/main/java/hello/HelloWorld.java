508ee7a46bf2c9395a7cd09e56637dc4d87fce7d
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-22 14:02:37 +0200

    Building Java Projects with Maven : Declare Dependencies

--- building-java-projects-maven/src/main/java/hello/HelloWorld.java
+++ building-java-projects-maven/src/main/java/hello/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
         System.out.println("Hello world");
     }
 }
