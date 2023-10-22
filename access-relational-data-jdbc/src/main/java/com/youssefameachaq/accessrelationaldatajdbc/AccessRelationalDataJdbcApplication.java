bf06d7885d120d2d3b1f624a21f48ebca562c77b
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-22 19:10:29 +0200

    Accessing Relational Data using JDBC with Spring : Setup the project

---
+++ access-relational-data-jdbc/src/main/java/com/youssefameachaq/accessrelationaldatajdbc/AccessRelationalDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.youssefameachaq.accessrelationaldatajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessRelationalDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessRelationalDataJdbcApplication.class, args);
+	}
+
+}
---
+++ access-relational-data-jdbc/src/test/java/com/youssefameachaq/accessrelationaldatajdbc/AccessRelationalDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssefameachaq.accessrelationaldatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessRelationalDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
