db3310ea4028a92749f13523120a808e2607d413
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-22 19:13:26 +0200

    Accessing Relational Data using JDBC with Spring : Create a Customer Object

---
+++ access-relational-data-jdbc/src/main/java/com/youssefameachaq/accessrelationaldatajdbc/models/Customer.java
@@ -0,0 +1,43 @@
+package com.youssefameachaq.accessrelationaldatajdbc.models;
+
+public class Customer {
+    private Long id;
+    private String firstName, lastName;
+
+    public Customer(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer {id: %d, firstName: %s, lastName: %s}",
+                this.id, this.firstName, this.lastName);
+    }
+}
