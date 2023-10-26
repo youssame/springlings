b7c811d9d3a2751807ee952e791ee95c08ffaf49
Youssef Ameachaq <youssef.ameachaq@orange.com>
2023-10-26 10:51:36 +0200

    Messaging with Redis : setup the project

---
+++ messaging-redis/src/test/java/com/youssefameachaq/messagingredis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssefameachaq.messagingredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
